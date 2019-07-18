b8eabbf01e88630afd9f81ee056db16676965bf4
Gabriel Broitman Pinheiro <gabriel.broitman@bndes.gov.br>
2019-07-18 17:34:49 -0300

    Ajuste classes teste

---
+++ src/main/java/com/weedem/jsaday/model/Droga.java
@@ -0,0 +1,62 @@
+package com.weedem.jsaday.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Droga {
+
+	private Long id;
+
+	private String nome;
+
+	private String descricao;
+
+	private Categoria categoria;
+	
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public byte[] getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(byte[] imagem) {
+		this.imagem = imagem;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	private byte[] imagem;
+
+}
